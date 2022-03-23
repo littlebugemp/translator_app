aad56330ef50a99ed934a50acbf86a266f7d7a1d
LittleBugEmpire <littlebugempire@gmail.com>
2022-03-23 23:25:31 +0530

    added home page

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,9 @@ from django.views import generic
 
 # Create your views here.
 
-class BlogView(generic.DetailView):
+class BlogView(generic.DetailView): #if need to render model data
     model = Post
     template_name = 'blog.html'
+
+class HomeView(generic.TemplateView): #only render to template without model
+    template_name = 'index.html'
