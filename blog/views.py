b729121147f9dde66c1d4871d2977942702cac01
LittleBugEmpire <littlebugempire@gmail.com>
2022-03-23 23:45:18 +0530

    added about page

--- blog/views.py
+++ blog/views.py
@@ -8,5 +8,9 @@ class BlogView(generic.DetailView): #if need to render model data
     model = Post
     template_name = 'blog.html'
 
-class HomeView(generic.TemplateView): #only render to template without model
-    template_name = 'index.html'
+class AboutView(generic.TemplateView): #only render to template without model
+    template_name = 'about.html'
+
+class PostList(generic.ListView): #rendering multiple data rows ex post
+    queryset = Post.objects.filter(status=1).order_by('date_created')
+    template_name = "index.html"
